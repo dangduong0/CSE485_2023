8c77b7cad8b53c604edc896c90410de54686a2cc
Vũ Thành Công <104372944+vuthanhcong02@users.noreply.github.com>
2023-05-20 21:44:53 +0700

    build login form with html,css

---
+++ BTTH02/src/login/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href="../../public/css/login.css" rel="stylesheet">
+    <title>Login</title>
+</head>
+<body>
+
+<div class="logo"></div>
+<div class="login-block">
+    <form>
+        <h1>Login</h1>
+        <input type="text" value="" placeholder="Username" id="username" />
+        <input type="password" value="" placeholder="Password" id="password" />
+        <button>Login</button>
+    </form>
+</div>
+</body>
+</html>
