d3fb5c2ad393f8ea9104cf4bffdfa471cfbe9d66
Vũ Thành Công <104372944+vuthanhcong02@users.noreply.github.com>
2023-05-21 15:38:27 +0700

    change navbar href

--- BTTH02/src/admin/admin_Class/actions/edit.php
+++ BTTH02/src/admin/admin_Class/actions/edit.php
@@ -1,6 +1,4 @@
 <?php
-include '../../sidebar.php';
-include '../../header.php';
 include '../../../../config/database.php';
 include '../../../../includes/function.php';
 $id = $_GET['id'];
@@ -10,6 +8,32 @@ $sqlMa_khoahoc = "SELECT * FROM courses";
 $courses = pdo($pdo, $sqlMa_khoahoc)->fetchAll();
 $sql_teacher = "SELECT id ,name FROM teachers";
 $teachers = pdo($pdo, $sql_teacher)->fetchAll();
+?>
+<nav class="w3-sidebar w3-collapse bg-black w3-animate-left" style="z-index:3;width:300px;" id="mySidebar"><br>
+    <div class="w3-container w3-row">
+        <div class="w3-col s8 w3-bar">
+            <span>Welcome, <strong>Admin</strong></span><br>
+        </div>
+    </div>
+    <hr>
+    <div class="w3-container">
+        <h5>Dashboard</h5>
+    </div>
+    <div class="w3-bar-block ">
+        <a href="../../index.php" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-users fa-fw"></i>   Overview</a>
+        <a href="../../admin_Student/index.php" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-users fa-fw"></i>  Sinh viên</a>
+        <a href="../../admin_Teacher/index.php" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-user fa-fw"></i>  Giáo viên</a>
+        <a href="../../admin_Course/index.php" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-book fa-fw"></i>  Khóa học</a>
+        <a href="../../admin_Class/index.php" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-diamond fa-fw"></i>  Lớp học phần</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-bell fa-fw"></i>  News</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-bank fa-fw"></i>  Thống kê</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-history fa-fw"></i>  404 Page</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-cog fa-fw"></i>  Settings</a><br><br>
+    </div>
+</nav>
+<?php 
+include '../../../../public/template/header.html';
+
 ?>
 <div class="container p-5">
     <form action="../actions/update.php" method="post">
@@ -58,5 +82,5 @@ $teachers = pdo($pdo, $sql_teacher)->fetchAll();
     </form>
 </div>
 <?php
-include '../../footer.php';
+include '../../../../public/template/footer.html';
 ?>
--- BTTH02/src/admin/admin_Teacher/actions/edit.php
+++ BTTH02/src/admin/admin_Teacher/actions/edit.php
@@ -1,11 +1,35 @@
 <?php
-include '../../sidebar.php';
-include '../../header.php';
 include '../../../../config/database.php';
 include '../../../../includes/function.php';
 $id =$_GET['id'];
 $sql = "SELECT *FROM teachers WHERE id=:id";
 $teacher = pdo($pdo,$sql,[$id])->fetch();
+
+?>
+<nav class="w3-sidebar w3-collapse bg-black w3-animate-left" style="z-index:3;width:300px;" id="mySidebar"><br>
+    <div class="w3-container w3-row">
+        <div class="w3-col s8 w3-bar">
+            <span>Welcome, <strong>Admin</strong></span><br>
+        </div>
+    </div>
+    <hr>
+    <div class="w3-container">
+        <h5>Dashboard</h5>
+    </div>
+    <div class="w3-bar-block ">
+        <a href="../../index.php" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-users fa-fw"></i>   Overview</a>
+        <a href="../../admin_Student/index.php" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-users fa-fw"></i>  Sinh viên</a>
+        <a href="../../admin_Teacher/index.php" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-user fa-fw"></i>  Giáo viên</a>
+        <a href="../../admin_Course/index.php" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-book fa-fw"></i>  Khóa học</a>
+        <a href="../../admin_Class/index.php" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-diamond fa-fw"></i>  Lớp học phần</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-bell fa-fw"></i>  News</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-bank fa-fw"></i>  Thống kê</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-history fa-fw"></i>  404 Page</a>
+        <a href="#" class="w3-bar-item w3-button w3-padding p-3"><i class="fa fa-cog fa-fw"></i>  Settings</a><br><br>
+    </div>
+</nav>
+<?php 
+include '../../../../public/template/header.html';
 
 ?>
 <div class="container p-5">
@@ -29,5 +53,5 @@ $teacher = pdo($pdo,$sql,[$id])->fetch();
     </form>
 </div>
 <?php
-include '../../footer.php';
+include '../../../../public/template/footer.html';
 ?>
